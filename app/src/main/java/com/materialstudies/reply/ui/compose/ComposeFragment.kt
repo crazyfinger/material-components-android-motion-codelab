c258936fc3018c6b7567fbf46b65e96f3036d666
Andrew <>
2021-08-20 17:04:30 +0800

    Add Container Transform transition from email address chip to card view

--- app/src/main/java/com/materialstudies/reply/ui/compose/ComposeFragment.kt
+++ app/src/main/java/com/materialstudies/reply/ui/compose/ComposeFragment.kt
@@ -27,6 +27,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.transition.Slide
+import androidx.transition.TransitionManager
 import com.google.android.material.transition.MaterialContainerTransform
 import com.materialstudies.reply.R
 import com.materialstudies.reply.data.Account
@@ -148,7 +149,14 @@ class ComposeFragment : Fragment() {
         closeRecipientCardOnBackPressed.expandedChip = chip
         closeRecipientCardOnBackPressed.isEnabled = true
 
-        // TODO: Set up MaterialContainerTransform beginDelayedTransition.
+        val transform = MaterialContainerTransform().apply {
+            startView = chip
+            endView = binding.recipientCardView
+            scrimColor = Color.TRANSPARENT
+            endElevation = resources.getDimension(R.dimen.email_recipient_card_popup_elevation_compat)
+            addTarget(binding.recipientCardView)
+        }
+        TransitionManager.beginDelayedTransition(binding.composeConstraintLayout,transform)
         binding.recipientCardView.visibility = View.VISIBLE
         // Using INVISIBLE instead of GONE ensures the chip's parent layout won't shift during
         // the transition due to chips being effectively removed.
@@ -164,7 +172,14 @@ class ComposeFragment : Fragment() {
         closeRecipientCardOnBackPressed.expandedChip = null
         closeRecipientCardOnBackPressed.isEnabled = false
 
-        // TODO: Set up MaterialContainerTransform beginDelayedTransition.
+        val transform = MaterialContainerTransform().apply {
+            startView = binding.recipientCardView
+            endView = chip
+            startElevation = resources.getDimension(R.dimen.email_recipient_card_popup_elevation_compat)
+            scrimColor = Color.TRANSPARENT
+            addTarget(chip)
+        }
+        TransitionManager.beginDelayedTransition(binding.composeConstraintLayout,transform)
         chip.visibility = View.VISIBLE
         binding.recipientCardView.visibility = View.INVISIBLE
     }
